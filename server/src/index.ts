181061fa5da9215dea67947e3e9de2d7895b4559
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-13 23:10:31 -0600

    @server: Set up dotenv library

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import app from "./server";
 import { PORT } from "./config";
+import dotenv from "dotenv";
+dotenv.config();
 
 app.listen(PORT, () => {
   console.log(`Server listening on Port: ${PORT}`);
