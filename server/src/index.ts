642d567ff6a32422b486ea1444f395fdac643355
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-14 00:40:02 -0600

    (server): fix connection to mongo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,6 @@
 import app from "./server";
 import { PORT } from "./config";
 import connectMongoDB from "./mongo";
-import dotenv from "dotenv";
-dotenv.config();
 
 app.listen(PORT, () => {
   void connectMongoDB(); // Creates the connection with MongoDB
