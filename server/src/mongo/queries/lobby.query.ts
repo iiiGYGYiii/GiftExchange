f5fe89f02b762e3fe56c5bcfb81a2a355691d8af
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-14 16:41:28 -0600

    (server): Add query to get matched participant

--- server/src/mongo/queries/lobby.query.ts
+++ server/src/mongo/queries/lobby.query.ts
@@ -1,7 +1,10 @@
 import { Lobby } from "../models";
 import { LobbyType } from "../models/Lobby.model";
 import { createIdentifier } from "../../utils/identifier";
-import { shuffleExchangeList } from "../../utils/exchange";
+import {
+  getMatchedParticipant,
+  shuffleExchangeList,
+} from "../../utils/exchange";
 
 export async function createLobby(
   lobbyOwner: string,
@@ -21,6 +24,32 @@ export async function createLobby(
   } catch (error) {
     return {
       error: "There ocurred and error, try again later.",
+    };
+  }
+}
+
+export async function fetchMatchedParticipant(
+  participant: string,
+  lobbyId: string
+): Promise<string | { errorMsg: string }> {
+  try {
+    const lobby = await Lobby.findById(lobbyId);
+    if (!lobby?.participants.includes(participant))
+      throw new Error("Participant doesn't exist");
+    const matchedParticipant = getMatchedParticipant(
+      lobby.participants,
+      lobby.shuffledList,
+      participant
+    );
+    return matchedParticipant;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errorMsg: error.message,
+      };
+    }
+    return {
+      errorMsg: "Something bad happen! Try again later",
     };
   }
 }
