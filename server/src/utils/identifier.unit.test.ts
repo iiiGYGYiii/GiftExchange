fbf14970976a5bdf85c442242eb6001e06353061
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-14 01:07:56 -0600

    (server): Add test for creating an identifier

---
+++ server/src/utils/identifier.unit.test.ts
@@ -0,0 +1,20 @@
+import { createIdentifier, limitStringToSizeNine } from "./identifier";
+
+describe("Identifier Test", () => {
+  it("Identifier must return a string", () => {
+    const newIdentifier = createIdentifier();
+    expect(typeof newIdentifier).toBe("string");
+  });
+
+  it("Identifier must have length of 9", () => {
+    const newIdentifier = createIdentifier();
+    expect(newIdentifier).toHaveLength(9);
+  });
+
+  it("Identifier always to length of 9", () => {
+    const oneDigit = 0;
+    expect(limitStringToSizeNine(oneDigit)).toHaveLength(9);
+    const tenDigits = 9999999999;
+    expect(limitStringToSizeNine(tenDigits)).toHaveLength(9);
+  });
+});
