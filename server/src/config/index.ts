c2c7466d7f47c25f731c778b488c3fe39802b5a8
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-14 23:00:52 -0600

    (server): Configure environment to dispatch client app

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,8 +1,17 @@
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 3001;
+export const PORT =
+  process.env.NODE_ENV === "development" ? 3001 : process.env.PORT || 3000;
 export const MONGO_URI =
   process.env.NODE_ENV === "development"
     ? process.env.MONGO_DEV_URI
     : process.env.MONGO_PROD_URI;
+const cwd = process.cwd().split("\\");
+cwd.pop();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export const PWD =
+  process.env.NODE_ENV === "development"
+    ? path.join(cwd.join("\\"), "client/client/")
+    : path.join(process.cwd(), "build/client/");
