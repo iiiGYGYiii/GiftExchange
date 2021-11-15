60c514a528fc7e28cffbaddd57f14a7440b70b7e
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-15 11:03:24 -0600

    (client): Create a component folder, with header, footer

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import "./Footer.styles.scss";
+
+const Footer = () => (
+  <footer>
+    <p>
+      Made with ❤ by:{" "}
+      <a href="http://www.instagram.com/iiiGYGYiii">iiiGYGYiii</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
