b9d16b37038eccf2bfc354c69e4636d5c9a089de
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-16 15:58:11 -0600

    (client): Add Carousel reusable HOC component.

--- client/src/HOCS/CarouselHOC/index.tsx
+++ client/src/HOCS/CarouselHOC/index.tsx
@@ -1,5 +1,81 @@
 import "./Carousel.styles.scss";
 
-export default function CarouselHOC({ children }: { children: JSX.Element }) {
-  return <div className="carousel-container">{children}</div>;
+const idGenerator = (indx: string | number, forAnchor?: boolean): string =>
+  `${forAnchor ? "#" : ""}carousel_slide${indx}`;
+
+function CarouselItem({
+  children,
+  indx,
+  lastIndx,
+}: {
+  children: JSX.Element;
+  indx: number;
+  lastIndx?: number;
+}) {
+  return (
+    <li id={idGenerator(indx)} tabIndex={0} className="carousel-slide">
+      {children}
+      <div className="carousel-snapper" />
+      {indx === 0 && lastIndx ? (
+        <a href={idGenerator(lastIndx, true)} className="carousel-prev">
+          <i className="fas fa-chevron-left"></i>
+        </a>
+      ) : (
+        <a href={idGenerator(indx - 1, true)} className="carousel-prev">
+          <i className="fas fa-chevron-left"></i>
+        </a>
+      )}
+      {lastIndx && indx !== 0 ? (
+        <a href={idGenerator(0, true)} className="carousel-next">
+          <i className="fas fa-chevron-right"></i>
+        </a>
+      ) : (
+        <a href={idGenerator(indx + 1, true)} className="carousel-next">
+          <i className="fas fa-chevron-right"></i>
+        </a>
+      )}
+    </li>
+  );
+}
+
+function NavigationItem({ indx }: { indx: number }) {
+  return (
+    <li className="carousel__navigation-item">
+      <a href={idGenerator(indx, true)} className="carousel__navigation-button">
+        Go to slide {indx}
+      </a>
+    </li>
+  );
+}
+
+export default function CarouselHOC({ children }: { children: JSX.Element[] }) {
+  const itemsInChildren = children.length;
+  return (
+    <section className="carousel" aria-label="Home-Gallery">
+      <ol className="carousel-viewport">
+        {children.map((child, indx) => (
+          <CarouselItem
+            key={`carousel__item${indx}`}
+            indx={indx}
+            lastIndx={
+              indx === 0 || indx === itemsInChildren - 1
+                ? itemsInChildren - 1
+                : undefined
+            }
+          >
+            {child}
+          </CarouselItem>
+        ))}
+      </ol>
+      <aside className="carousel__navigation">
+        <nav>
+          <ol className="carousel__navigation-list">
+            {children.map((child, i) => (
+              <NavigationItem key={"nav" + idGenerator(i, true)} indx={i} />
+            ))}
+          </ol>
+        </nav>
+      </aside>
+    </section>
+  );
 }
