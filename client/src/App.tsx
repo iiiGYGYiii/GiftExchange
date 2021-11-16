fa7fa4daf3285627e5a7df694b05e61fbda69dd4
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-16 16:14:24 -0600

    (client): Add routing boilerplate.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
-import "./App.css";
+import "./App.scss";
+import { Header, Footer } from "./components/";
+import { HomePage } from "./pages";
+import { PageHOC } from "./HOCS";
+import { Route, Switch } from "wouter";
+import { routes } from "./utils/constants";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
+      <main>
+        <Switch>
+          <Route path={routes.lobby}>LOBBY ROUTE</Route>
+          <Route path={routes.searchLobby}>SEARCH LOBBY ROUTE</Route>
+          <Route path={routes.createLobby}>CREATE LOBBY ROUTE</Route>
+          <Route path={routes.home}>
+            <PageHOC title="interchanger">
+              <HomePage />
+            </PageHOC>
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
     </div>
   );
 }
