a9465daad020db4c5dce2f4f5231c1c48dd5e669
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-16 02:46:35 -0600

    (client): Update Card component: id and padding.

--- client/src/HOCS/CardHOC/index.tsx
+++ client/src/HOCS/CardHOC/index.tsx
@@ -3,12 +3,14 @@ import "./Card.styles.scss";
 export default function Card({
   title,
   children,
+  id,
 }: {
   title: string;
   children?: JSX.Element;
+  id: string;
 }) {
   return (
-    <div className="card">
+    <div id={id} className="card">
       <div className="card-title">
         <h1>{title}</h1>
       </div>
