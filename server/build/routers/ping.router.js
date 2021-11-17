bca5cde979368fa382ebe3e3266fcfaf05ec065d
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-17 14:25:54 -0600

    (main): Revert changes for merging with main branch

--- server/build/routers/ping.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var pingRouter = (0, express_1.Router)();
-pingRouter.route("/").get(function (_req, res) {
-    res.send("PONG").end();
-});
-exports.default = pingRouter;
