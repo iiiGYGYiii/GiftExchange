f395fde58e451d2ba6311559e0812ad5888896d3
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-16 23:39:29 -0600

    (client): Add Lobby Create Form Page.

--- client/src/pages/CreateLobby/index.tsx
+++ client/src/pages/CreateLobby/index.tsx
@@ -1,14 +1,70 @@
-import { FormEventHandler, useState } from "react";
-import { postLobbyData } from "../../utils/types";
+import useLobbyForm from "../../hooks/lobbyForm.hook";
+
 import "./CreateLobby.styles.scss";
 
-const initialLobbyData: postLobbyData = {
-  lobbyOwner: "",
-  participants: [],
-};
-
-const initialParticipantState = "";
+import {
+  TableForm,
+  TableInput,
+  Notification,
+  DisplayLobby,
+} from "../../components";
 
 export default function CreateLobby() {
-  return <div className="lobby-create-container"></div>;
+  const {
+    handleSubmit,
+    lobbyOwner,
+    handleOwnerKey,
+    handleOwnerChange,
+    handleOwnerClick,
+    participant,
+    handleParticipantKey,
+    handleParticipantChange,
+    handleParticipantClick,
+    participants,
+    ownerIsSet,
+    error,
+    lobbyId,
+  } = useLobbyForm();
+
+  if (lobbyId)
+    return (
+      <DisplayLobby
+        lobbyOwner={lobbyOwner}
+        lobbyId={lobbyId}
+        participants={participants}
+      />
+    );
+
+  return (
+    <div className="lobby-create-container">
+      {error ? <Notification message={error} error={true} /> : null}
+      <TableForm submitText="¡Crear Sala!" handleSubmit={handleSubmit}>
+        <>
+          <TableInput
+            textLabel="Organizador"
+            inputId="lobby-owner"
+            inputValue={lobbyOwner}
+            handleKey={handleOwnerKey}
+            handleChange={handleOwnerChange}
+            textButton="Declararse"
+            handleClick={handleOwnerClick}
+            disabled={ownerIsSet}
+            required={true}
+          />
+          <TableInput
+            textLabel="Participante"
+            inputId="participant"
+            inputValue={participant}
+            handleKey={handleParticipantKey}
+            handleChange={handleParticipantChange}
+            textButton="Añadir"
+            handleClick={handleParticipantClick}
+          />
+        </>
+      </TableForm>
+      {participants.map((p) => (
+        <p key={p}>{p}</p>
+      ))}
+    </div>
+  );
 }
