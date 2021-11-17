a7a4652c533610ec5a7df04b411756537ce3a80b
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-16 23:40:11 -0600

    (client): Add components created to index.

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,2 +1,6 @@
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
+export { default as TableForm } from "./TableForm";
+export { default as TableInput } from "./TableInput";
+export { default as Notification } from "./Notification";
+export { default as DisplayLobby } from "./DisplayLobby";
